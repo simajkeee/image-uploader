71704c6a502a0b84d21677693006dc003ca07358
simajkeee <37087698+simajkeee@users.noreply.github.com>
2022-08-28 18:56:18 +0300

    add new route /preview

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\ImageUploader;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ImageUploader::class, 'index']);
+Route::get('/preview', [ImageUploader::class, 'show']);
 Route::post('/upload', [ImageUploader::class, 'store']);
