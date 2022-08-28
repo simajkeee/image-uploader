d5a568fcc0fec77ff91638774508f12191878b9c
simajkeee <37087698+simajkeee@users.noreply.github.com>
2022-08-28 16:11:53 +0300

    add ImageUploader::store logic to store image

--- app/Http/Controllers/ImageUploader.php
+++ app/Http/Controllers/ImageUploader.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageUploader extends Controller
@@ -11,8 +12,26 @@ class ImageUploader extends Controller
         return view('imageUploader.index');
     }
 
-    public function store(Request $request)
+    public function store(Request $request): Image
     {
-        // upload images
+        if (!$request->file('image')) {
+            return response()->json(['err' => 'Image is not present.'], 400);
+        }
+
+        $request->validate(['image' => 'required|file|max:10000|image']);
+
+        $filepath = $request->file('image')->store('public/images');
+
+        if (!$filepath) {
+            return response()->json(['err' => 'Unable to store the file.'], 500);
+        }
+
+        $file = $request->file('image');
+
+        return Image::create([
+            'name' => $file->hashName(),
+            'extension' => $file->extension(),
+            'size' => $file->getSize(),
+        ]);
     }
 }
