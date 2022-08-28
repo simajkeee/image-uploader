a07926eb510d784a8e7ef0fca93b9e098ff96800
simajkeee <37087698+simajkeee@users.noreply.github.com>
2022-08-28 16:10:35 +0300

    fix route /upload in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ImageUploader;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ImageUploader::class, 'index']);
-Route::post('/upload', ImageUploader::class, 'store');
+Route::post('/upload', [ImageUploader::class, 'store']);
