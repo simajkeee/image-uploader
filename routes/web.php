c08c6d973128ef8d3fbaa81e4c12f05ff48eb62a
simajkeee <37087698+simajkeee@users.noreply.github.com>
2022-08-30 06:46:12 +0300

    change /preview to /images route path

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\ImageUploader;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ImageUploader::class, 'index']);
-Route::get('/preview', [ImageUploader::class, 'show']);
+Route::get('/images', [ImageUploader::class, 'show']);
 Route::post('/upload', [ImageUploader::class, 'store']);
