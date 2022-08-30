4dbd87f1453594d2e7ee7d6addff074732f5b8bd
simajkeee <37087698+simajkeee@users.noreply.github.com>
2022-08-30 06:45:51 +0300

    add bootstrap 5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
 </head>
 <body class="antialiased">
 <div class="container">
-    <h2>Form to upload images</h2>
-    @yield('content')
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-center mb-4">Form to upload images</h2>
+        </div>
+        @yield('content')
+    </div>
 </div>
 <script src="{{ mix("js/app.js") }}"></script>
 </body>
